9d70d11f0b7cca42b71a54176d0a11cd8c4acccf
Daani Faiz <dfaiz31@gmail.com>
2024-12-09 22:05:25 +1100

    Authorization of health data flow button added

--- health_tracker/ContentView.swift
+++ health_tracker/ContentView.swift
@@ -1,16 +1,41 @@
 import SwiftUI
 
+
 struct ContentView: View {
+    @State private var authorizationGranted = false
+    @State private var error: Error?
+
+    
+    
     var body: some View {
         VStack {
-            Text("Welcome to My First App!")
+            Text("Welcome to Pulse!")
                 .font(.largeTitle)
                 .padding()
-            Text("This is a simple iOS app.")
+            Text("Your health advisor to help you take control of your health")
                 .foregroundColor(.gray)
+            
+            Button("Request Authorization") {
+                            HealthKitManager.shared.requestAuthorization { success, error in
+                                self.authorizationGranted = success
+                                self.error = error
+                            }
+                        }
+                        
+                        if authorizationGranted {
+                            Text("HealthKit authorization granted.")
+                        } else if let error = error {
+                            Text("Authorization failed: \(error.localizedDescription)")
+                        } else {
+                            Text("Authorization not determined yet.")
+                        }
+            Button("Fetch Steps") {
+                HealthKitManager.shared.fetchStepCountData()
+            }
         }
     }
 }
+
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
